f62f123a6e754ea9d88b8d3dfb102c96a460a03c
Rob Gries <robert.gries@mu.edu>
2015-03-30 15:16:20 -0500

    Completed the Temp package

--- Compiler/src/Temp/Label.java
+++ Compiler/src/Temp/Label.java
@@ -1,5 +1,7 @@
 package Temp;
 
+import Symbol.Symbol;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jchitel
@@ -7,4 +9,20 @@ package Temp;
  * Time: 11:17 PM
  */
 public class Label {
+
+    private static int labelCount = 0;
+    private String name;
+
+    public Label() {
+        this.name = "L" + labelCount++;
+    }
+
+    public Label(String s) {
+        this.name = s;
+    }
+
+    public String toString() {
+        return name;
+    }
+
 }
--- Compiler/src/Temp/Temp.java
+++ Compiler/src/Temp/Temp.java
@@ -7,4 +7,16 @@ package Temp;
  * Time: 11:17 PM
  */
 public class Temp {
+
+    private static int regCount = 0;
+    private int regIndex;
+
+    public Temp() {
+        regIndex = regCount++;
+    }
+
+    public String toString() {
+        return "t" + Integer.toString(this.regIndex);
+    }
+
 }
