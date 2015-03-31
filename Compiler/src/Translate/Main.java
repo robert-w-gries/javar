ab155f3bfffdaba1054c9569b56978dc6bf57b8e
jchitel <jchitel@gmail.com>
2015-03-31 14:55:30 -0500

    Initial changes made to ReadTypes parser

--- Compiler/src/Translate/Main.java
+++ Compiler/src/Translate/Main.java
@@ -2,105 +2,59 @@
  * All rights reserved.  */
 
 package Translate;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
-
+import java.util.*;
 //import Semant.*;
 import Absyn.Program;
 import Parse.*;
-
-import java.util.HashMap; 
-import java.util.List;
-import java.util.LinkedList;
-
+import Parse.ParseException;
+import Semant.*;
 //import Translate.Translate;
 
-public class Main 
-{
-    static final String VERSION = "COSC 4400" +
-		" Concurrent MiniJava Intermediate Translator" +
-		" v1.09";
+public class Main {
 
-    public static void main(String args[])
-    {
-		java.io.Reader reader = null;
-		
-		for (int i = 0; i < args.length; i++)
-	    {
-			if (args[i].equals("-v"))
-		    {
-				System.out.println(VERSION);
-				System.exit(0);
-		    }
-			else if (args[i].equals("-"))
-		    {
-				java.io.InputStreamReader isr = 
-					new java.io.InputStreamReader(System.in);
-				reader = new java.io.BufferedReader(isr);
-		    }
-			else
-		    {
-				try
-			    {
-					reader = new java.io.FileReader(args[i]);
-			    }
-				catch (java.io.FileNotFoundException fnfe)
-			    {
-					System.err.println("File Not Found: " + args[i]);
-					System.exit(-1);
-			    }
-		    }
-	    }
-		
-		if (null == reader)
-	    {
-			System.err.println("Usage: Translate [-v] file.java");
-			System.exit(-2);
-	    }   
-		
-		Absyn.Program parse = null;
-		try
-	    {
-			parse = new Parse.ReadTypes(reader).Program();
-	    }
-		catch (ParseException p)
-	    {
-			System.err.println(p.toString());
-			System.exit(-1);
-	    }
-		
-		Translate translate = new Translate(new Mips.MipsFrame());
-		parse.accept(translate);
-		
-		PrintWriter writer = new PrintWriter(System.out);
-//	Semant.PrintVisitor pv =
-//	    new Semant.PrintVisitor(writer);
-//	pv.visit(parse);
-		
-		for (Iterator frags = translate.results().iterator(); 
-			 frags.hasNext(); ) 
-		{
-			Frag f = (Frag)frags.next();
-			if (f instanceof DataFrag) 
-			{
-				writer.println("DataFrag(");
-				writer.println(f);
-				writer.println(")");
-			} 
-			else 
-			{
-				writer.println("ProcFrag(");
-				ProcFrag p = (ProcFrag)f;
-				p.frame.printFrame(writer);
-				if (p.body != null) 
-				{
-					new Tree.Print(writer, p.body);
-					writer.println();
-				}
-				writer.println(")");
-			}
-		}  
-		writer.flush();
+	private static final String PARSE_FLAG = "--parse", CHECK_FLAG = "--check";
+
+	public static void main(String[] args) throws IOException, Parse.ParseException, Semant.ParseException {
+		String file = null;
+		boolean parse = false, check = false;
+
+		for (String arg : args) {
+			if (arg.equals(PARSE_FLAG)) parse = true;
+			else if (arg.equals(CHECK_FLAG)) check = true;
+			else if (file == null) file = arg;
+			else printUsageAndExit();
+		}
+
+		if (file == null) printUsageAndExit();
+
+		assert file != null; // this is to get rid of warning in intellij
+		FileReader reader = new FileReader(file);
+		Program program;
+
+		if (parse) {
+			new MiniJavaParser(reader);
+			program = MiniJavaParser.Goal();
+		} else {
+			new ReadAbsyn(reader);
+			program = ReadAbsyn.Program();
+		}
+
+		// run program through the type checker
+		TypeChecker typeChecker = new TypeChecker();
+		typeChecker.visit(program);
+
+		// print the program back out
+		PrintVisitor visitor = new PrintVisitor();
+		visitor.visit(program);
 	}
-}	
+
+	private static void printUsageAndExit() {
+		System.err.println("Invalid usage");
+		System.err.println("\tUsage: java Translate.Main [--parse] [--check] file");
+		System.exit(0);
+	}
+}
 
