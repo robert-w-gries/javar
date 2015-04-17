4dc1f0dc761dae5a5d4c5db0747d29d5096c2a84
Rob Gries <robert.gries@mu.edu>
2015-04-17 16:37:59 -0500

    Added documentation of broken testcases

---
+++ Compiler/tests/AssemTests/brokenArrayLengthField.java
@@ -0,0 +1,23 @@
+class BinarySearch{
+    public static void main(String[] a){
+        Xinu.printint(new BS().Start(20));
+    }
+}
+// This class contains an array of integers and
+// methods to initialize, print and search the array
+// using Binary Search
+
+class BS{
+
+    int[] number ;
+
+    public int Start(int x) {
+
+        int size;
+        size = number.length;
+
+        return size;
+
+    }
+
+}
