f4c7ec6e09385c0e1ca9ad1fb786748899a41ca4
jchitel <jchitel@gmail.com>
2015-04-27 22:35:01 -0500

    Fixed up ReadAssem parser

--- Compiler/src/Translate/ProcFrag.java
+++ Compiler/src/Translate/ProcFrag.java
@@ -1,5 +1,9 @@
 package Translate;
 
+import Assem.Instr;
+
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jchitel
@@ -10,10 +14,15 @@ public class ProcFrag extends Frag {
 
     public Tree.Stm body;
     public Frame.Frame frame;
+    public List<Instr> instructions;
 
     public ProcFrag(Tree.Stm body, Frame.Frame frame) {
         this.body = body;
         this.frame = frame;
     }
 
+    public ProcFrag(List<Instr> instructions, Frame.Frame frame) {
+        this.instructions = instructions;
+        this.frame = frame;
+    }
 }
